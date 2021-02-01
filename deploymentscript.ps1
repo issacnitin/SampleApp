ffa44cdbe23753526dd20c057b5a9996a7c7154a
Nitin Issac Joy <nijoy@microsoft.com>
2021-02-01 19:29:38 +0530

    Updated deployment script

--- deploymentscript.ps1
+++ deploymentscript.ps1
@@ -1,8 +1,46 @@
-﻿# az login --use-device-code
+﻿function Get-UrlStatusCode([string] $Url)
+{
+    try
+    {
+        (Invoke-WebRequest -Uri $Url -UseBasicParsing -DisableKeepAlive).StatusCode
+    }
+    catch [Net.WebException]
+    {
+        [int]$_.Exception.Response.StatusCode
+    }
+}
+
+# az login --use-device-code
 $output = az account show | ConvertFrom-Json
-Write-Host "Currently logged in to subscription" $output.id " in tenant " $output.tenantId
-$deploymentName = Read-Host -Prompt "Enter webapp name"
+$subscriptionList = az account list | ConvertFrom-Json 
+$subscriptionList | Format-Table name, id, tenantId -AutoSize
+$selectedSubscription = $output.name
+Write-Host "Currently logged in to subscription" $output.name " in tenant " $output.tenantId
+$selectedSubscription = Read-Host "Enter subscription Id ("$output.id")"
+if([string]::IsNullOrWhiteSpace($selectedSubscription)) {
+    $selectedSubscription = $output.id
+} else {
+    az account set --subscription $selectedSubscription
+    Write-Host "Changed to subscription ("$selectedSubscription")"
+}
+
+while($true) {
+    $deploymentName = Read-Host -Prompt "Enter webapp name"
+    # Create the request
+    $HTTP_Status = Get-UrlStatusCode('http://' + $deploymentName + '.azurewebsites.net')
+    if($HTTP_Status -eq 0) {
+        Write-Host "Webapp name available"
+        break
+    } else {
+        Write-Host "Webapp name taken"
+    }
+}
+
 $location = Read-Host -Prompt "Enter location (eastus)"
+if([string]::IsNullOrWhiteSpace($location)) {
+    $location = "eastus"
+}
+
 $resourceGroup = $deploymentName + "-rg"
 Write-Host "Creating resource group " $resourceGroup
 az group create --location $location --name $resourceGroup
@@ -11,10 +49,13 @@ $databaseName = $deploymentName + "db"
 Write-Host "Deploying Sample application.. (this might take a few minutes)"
 $deploymentOutputs = az deployment group create --resource-group $resourceGroup --mode Incremental --template-file ./windows-webapp-template.json --parameters "webAppName=$deploymentName" --parameters "hostingPlanName=$deploymentName-host" --parameters "appInsightsLocation=$location" --parameters "sku=P1V2 Premium" --parameters "databaseAccountId=$databaseName" --parameters "databaseAccountLocation=$location"
 $deploymentOutputs = $deploymentOutputs | ConvertFrom-Json
-$connectionString=$deploymentOutputs.properties.outputs.azureCosmosDBAccountKeys.value.connectionStrings[0].connectionString
+$connectionString = $deploymentOutputs.properties.outputs.azureCosmosDBAccountKeys.value.connectionStrings[0].connectionString
 
-az webapp config appsettings set --name $deploymentName --resource-group $resourceGroup --settings CONNECTION_STRING="$connectionString"
-az webapp config appsettings set --name $deploymentName --resource-group $resourceGroup --settings MSDEPLOY_RENAME_LOCKED_FILES=1
+Write-Host "Setting connection string to cosmos db"
+$setConnectionString = az webapp config appsettings set --name $deploymentName --resource-group $resourceGroup --settings CONNECTION_STRING="$connectionString"
+
+Write-Host "Setting app setting for App Service"
+$setAppSettings = az webapp config appsettings set --name $deploymentName --resource-group $resourceGroup --settings MSDEPLOY_RENAME_LOCKED_FILES=1
 
 $publishConfig = az webapp deployment list-publishing-credentials --name $deploymentName --resource-group $resourceGroup | ConvertFrom-Json
 
@@ -25,7 +66,6 @@ git commit -m "Initial commit"
 git remote add azwebapp $publishConfig.scmUri
 git push azwebapp master
 
-Start-Process "https://$deploymentName.azurewebsites.net"
-
 Write-Host "Deployment Complete"
+Write-Host "Open url https://$deploymentName.azurewebsites.net in the browser"
 Write-Host "To delete the app, run command 'az group delete $resourceGroup'"
